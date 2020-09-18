dab3ff789a3b4fee64f38e452d97d527b69a356b
Christy Hauschild <christyhauschid@gmail.com>
2020-09-17 23:29:59 -0300

    test: heading component

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -1,5 +1,6 @@
 import styled, { css, DefaultTheme } from 'styled-components'
 import media from 'styled-media-query'
+
 import { HeadingProps } from '.'
 
 const wrapperModifiers = {
@@ -11,11 +12,10 @@ const wrapperModifiers = {
   lineBottom: (theme: DefaultTheme) => css`
     position: relative;
     margin-bottom: ${theme.spacings.medium};
-
     &::after {
       position: absolute;
       left: 0;
-      bottom: -1rem;
+      bottom: -0.5rem;
       content: '';
       width: 5rem;
       border-bottom: 0.5rem solid ${theme.colors.primary};
@@ -25,14 +25,13 @@ const wrapperModifiers = {
 
 export const Wrapper = styled.h2<HeadingProps>`
   ${({ theme, color, lineLeft, lineBottom }) => css`
-    color: ${theme.colors[color!]}
+    color: ${theme.colors[color!]};
     font-size: ${theme.font.sizes.xlarge};
-
     ${media.greaterThan('medium')`
       font-size: ${theme.font.sizes.xxlarge};
     `}
-
     ${lineLeft && wrapperModifiers.lineLeft(theme)}
-    ${lineBottom && wrapperModifiers.lineBottom(theme)}
+    ${lineBottom &&
+    wrapperModifiers.lineBottom(theme)}
   `}
 `
